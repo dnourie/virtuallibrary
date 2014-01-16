1d6576de3de8078e256fc9c0ad04c434ec40bef5
dnourie2 <dananourie@gmail.com>
2014-01-15 21:32:17 -0800

    Cleand up files, used sass

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :borrow, :edit, :update, :destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
   # GET /books.json
