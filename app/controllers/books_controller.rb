441e32aa7dda5a763941c2c4fb1e19996e42fd13
dnourie2 <dananourie@gmail.com>
2014-01-17 17:21:53 -0800

    changed logic on borrow

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -45,7 +45,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.html { redirect_to @book, notice: 'Your book was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
